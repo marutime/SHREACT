4b8c3667da030c0388177a044b2c4464b134a5bc
Hojae Lee <rugbyhj99@naver.com>
2024-11-23 12:55:25 +0900

    contact change 1

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,8 +3,37 @@ import './Contact.scss';
 
 const Contact = () => {
   return (
-    <div>Contact
-      asdkfjakl
+    <div className='contact-container'>
+      <form className='contact-form'>
+        <div>
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"            
+            style={{ backgroundColor: '#e0e0e0', borderColor: '#ccc', padding: '10px', marginBottom: '10px' }}
+          />
+          
+        </div>
+        <div>
+          <label>E-mail</label>
+          <input
+            type="email"
+            name="email"
+            style={{ backgroundColor: '#e0e0e0', borderColor: '#ccc', padding: '10px', marginBottom: '10px' }}
+          />
+          
+        </div>
+        <div>
+          <label>Message</label>
+          <textarea name="" id=""></textarea>
+        </div>
+        <div>
+          <button type="submit" style={{ backgroundColor: '#ccc', padding: '10px' }}>
+            submit
+          </button>
+        </div>
+      </form>
+
     </div>
   )
 }
